73c5a67145573045fbb66eb904a328324b149a36
Rizky Ajie Kurniawan - Mac <rizkyak994@gmail.com>
2023-01-18 16:15:20 +0700

    clear template hello world

--- diary/src/App.vue
+++ diary/src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="KITA LANJUT LAGI BESOK ! BISMILLAH"/>
+<CardDiary msg="TODOS"></CardDiary>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import CardDiary from './components/CardDiary.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CardDiary
   }
 }
 </script>
---
+++ diary/src/components/CardDiary.vue
@@ -0,0 +1,39 @@
+<template>
+    <di>
+      <h1>{{ msg }}</h1>
+      <ul>
+        <li v-for="todo of todos" :key="todo.id">
+          {{ todo.name }}
+        </li>
+      </ul>
+    </di>
+  </template>
+
+<script>
+import axios from "axios"
+export default {
+    name: 'DiaryJajanCardDiary',
+    props: {
+        msg: String
+    },
+
+    data() {
+    return {
+      todos: []
+    };
+  },
+  async created() {
+    try {
+      const res = await axios.get(`http://localhost:3001/todos`);
+
+      this.todos = res.data;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
