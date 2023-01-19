671bd8a7108bb0f3b39c9373234be6ea64eb8ee1
Rizky Ajie Kurniawan - Mac <rizkyak994@gmail.com>
2023-01-19 16:23:17 +0700

    update

--- diary/src/store/index.js
+++ diary/src/store/index.js
@@ -1,46 +1,55 @@
 import { createStore } from "vuex";
+import axios from "axios";
 
 const store = createStore({
-    state:{
-        dataListing: [],
-        subTotal: 0
+  state: {
+    dataListing: [],
+    subTotal: 0,
+  },
+  getters: {
+    currentListing: (state) => state.dataListing,
+    currentSubtotal: (state) => state.subTotal,
+  },
+  mutations: {
+    SET_DATA(state, payload) {
+      state.dataListing = payload;
     },
-    getters:{
-        currentListing: (state) => state.dataListing,
-        currentSubtotal: (state) => state.subTotal
+    SET_SUBTOTAL(state, payload) {
+      state.subTotal = payload;
     },
-    mutations:{
-        SET_DATA(state, payload){
-            state.dataListing = payload
-        },
-        SET_SUBTOTAL(state, payload){
-            state.subTotal = payload
-        },
-        ADD_DATA(state, payload) {
-            let data = state.dataListing
-            let newData = payload
-            let key = newData.tanggal
-            var newArray = data[key] ? [...data[key],newData] : [newData];
-            Object.assign(data, {[key]:newArray});
-            state.dataListing = data
-          }
+    ADD_DATA(state, payload) {
+      let data = state.dataListing;
+      let newData = payload;
+      let key = newData.tanggal;
+      var newArray = data[key] ? [...data[key], newData] : [newData];
+      Object.assign(data, { [key]: newArray });
+      state.dataListing = data;
     },
-    actions:{
-        async getData(commit, payload){
-           let result = payload.reduce(function (r, a){
-            r[a.tanggal] = r[a.tanggal] || [];
-            r[a.tanggal].push(a);
-            return r;
-           }, Object.create(null))
-           store.commit("SET_DATA", result)
-        },
-        async getCalc(commit, payload){
-           store.commit("SET_SUBTOTAL", payload)
-        },
-        addData({ commit }, payload) {
-            commit('ADD_DATA', payload)
-          }
-    }
-})
+  },
+  actions: {
+    async getData() {
+      try {
+        const res = await axios.get(`http://localhost:3000/items`);
 
-export default store
+        let payload = res.data;
+
+        let result = payload.reduce(function (r, a) {
+          r[a.tanggal] = r[a.tanggal] || [];
+          r[a.tanggal].push(a);
+          return r;
+        }, Object.create(null));
+        store.commit("SET_DATA", result);
+      } catch (e) {
+        console.error(e.error);
+      }
+    },
+    async getCalc(commit, payload) {
+      store.commit("SET_SUBTOTAL", payload);
+    },
+    addData({ commit }, payload) {
+      commit("ADD_DATA", payload);
+    },
+  },
+});
+
+export default store;
